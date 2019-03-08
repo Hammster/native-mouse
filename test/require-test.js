30d52675a7479bee0448b871af69872baaa4e0c4
Hans Koch <mail@hans-koch.me>
2019-03-08 21:54:48 +0100

    created tests for importing the library and attaching events

---
+++ test/require-test.js
@@ -0,0 +1,19 @@
+const mouse = require('../')
+
+const events = [
+    'left-down',
+    'left-up',
+    'right-down',
+    'right-up',
+    'move',
+    'left-drag',
+    'right-drag'
+]
+
+for (const event of events) {
+    mouse.on(event, function(x, y) {
+        console.log(x, y)
+    })
+}
+
+// @TODO trigger mouse automation
