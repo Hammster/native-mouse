05e8ae80597b26a392f9589cd6a3330c27a9134a
Hans Koch <mail@hans-koch.me>
2019-03-09 01:05:19 +0100

    added propert typescript support

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,28 @@
-import { EventEmitter } from "events";
+/// <reference types="node" />
 
-enum NativeMouseEventType {
-    'left-down',
-    'left-up',
-    'right-down',
-    'right-up',
-    'move',
-    'left-drag',
-    'right-drag'
+declare module 'native-mouse' {
+    import { EventEmitter } from "events";
+
+    export enum NativeMouseEventType {
+        LEFT_DOWN = 'left-down',
+        LEFT_UP = 'left-up',
+        RIGHT_DOWN = 'right-down',
+        RIGHT_UP = 'right-up',
+        MOVE = 'move',
+        LEFT_DRAG = 'left-drag',
+        RIGHT_DRAG = 'right-drag'
+    }
+
+    export type NativeMouseEventHandler = (type: NativeMouseEventType, x: number , y: number) => void
+
+    class NativeMouseInstance extends EventEmitter {
+        unref: () => void
+        ref: () => void
+        destroy: () => void
+
+        on(event: NativeMouseEventType, listener: NativeMouseEventHandler): this;
+    }
+
+    const _default: NativeMouseInstance;
+    export default _default;
 }
-
-type NativeMouseEventHandler = (type: NativeMouseEventType, x: number , y: number) => void
-
-interface INativeMouseEvent {
-    on(event: NativeMouseEventType, listener: NativeMouseEventHandler): this;
-}
-
-class NativeMouseEventEmitter extends EventEmitter implements INativeMouseEvent {
-
-}
-
-export default NativeMouseEventEmitter
